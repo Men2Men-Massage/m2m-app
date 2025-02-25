16067588d8faf7caafe4dbeb90afad10b60419a1
Men2Men-Massage <info@men2men.center>
2025-02-25 19:35:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -98,6 +98,7 @@
     // Check authentication on app startup
     if (!checkAuth()) {
         document.getElementById('auth-overlay').style.display = 'flex';
+        return false;
     }
 
     // Function to calculate the payment
@@ -190,7 +191,7 @@
         });
 
         let tbody = calendarTable.createTBody();
-        let date = 0;
+        let date = 1;
         let dayOfWeekCounter = startingDayOfWeek === 0 ? 6 : startingDayOfWeek - 1;
 
         for (let i = 0; i < 6; i++) {
@@ -199,15 +200,15 @@
                 if (i === 0 && j < dayOfWeekCounter) {
                     let cell = row.insertCell();
                     cell.textContent = '';
-                } else if (date >= daysInMonth) {
+                } else if (date > daysInMonth) {
                     break;
                 } else {
-                    date++;
                     let cell = row.insertCell();
                     cell.classList.add('day');
                     let dayNumber = document.createElement('div');
                     dayNumber.classList.add('day-number');
                     dayNumber.textContent = date;
+
                     const currentDate = new Date(year, month, date, 0, 0, 0, 0);
                     const currentDateString = currentDate.toISOString().split('T')[0];
 
@@ -227,6 +228,7 @@
                             showDailyPayments(currentDateStringForEvent);
                         });
                     })(currentDateString);
+                    date++;
                 }
             }
         }
