6f50c08d7a85f21d613e299699f4145b83c22915
Men2Men-Massage <info@men2men.center>
2025-03-04 19:10:12 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" 
+        />
         <meta name="theme-color" content="#F5F5F5" />
         <link rel="stylesheet" href="/style.css" />
       </Head>
