9bc714febd486c99d8fd8d3fea250e56667fa8b9
Men2Men-Massage <info@men2men.center>
2025-03-04 19:09:50 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,10 @@ const Home: NextPage = () => {
       <Head>
         <title>M2M Payment Calculator</title>
         <meta name="description" content="Payment calculator for M2M" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" 
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="Payment Calc" />
